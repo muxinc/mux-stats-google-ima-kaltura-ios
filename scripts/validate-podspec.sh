6f8d2b173d77f7737e82b507cfcabbde3d856381
AJ Lauer Barinov <abarinov@mux.com>
2023-09-20 16:26:51 -0700

    ci: remove alternate spec source scaffolding

--- scripts/validate-podspec.sh
+++ scripts/validate-podspec.sh
@@ -15,21 +15,13 @@ echo "▸ Cleaning Cocoapods Cache"
 
 pod cache clean --all
 
-echo "▸ Adding Private Pod Repo"
-
-pod repo add andrewjl-mux-specs https://github.com/andrewjl-mux/Specs.git
-
 echo "▸ Updating Local Pod Repos"
 
 pod repo update
 
-echo "▸ Linting Local Private Pod Repo"
-
-pod repo lint /Users/runner/.cocoapods/repos/andrewjl-mux-specs
-
 echo "▸ Validating Podspec"
 
-pod lib lint --allow-warnings --verbose --sources=https://cdn.cocoapods.org/,https://github.com/andrewjl-mux/Specs.git
+pod lib lint --allow-warnings --verbose
 
 echo "▸ Back to Xcode 14.3.1"
 
